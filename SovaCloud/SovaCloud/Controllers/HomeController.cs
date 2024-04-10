d23c5f9a6a92f14963dd87e174880d3d2a5c2902
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-04-10 11:36:05 +0200

    Add based Sign In form

--- SovaCloud/SovaCloud/Controllers/HomeController.cs
+++ SovaCloud/SovaCloud/Controllers/HomeController.cs
@@ -6,27 +6,19 @@ namespace SovaCloud.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult YourFiles()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult SignIn()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
--- SovaCloud/SovaCloud/Program.cs
+++ SovaCloud/SovaCloud/Program.cs
@@ -4,23 +4,23 @@ using Amazon.SecretsManager.Model;
 using Microsoft.EntityFrameworkCore;
 using SovaCloud.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.EUCentral1);
 
+IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.EUCentral1);
 GetSecretValueRequest request = new GetSecretValueRequest
 {
     SecretId = "Development_SovaCloudStorage_Database__ConnectionString"
 };
 
 GetSecretValueResponse response = await client.GetSecretValueAsync(request);
-
-string connectionStringSecret = response.SecretString;
+string connectionStringSecretValue = response.SecretString;
 
 builder.Services.AddDbContext<SovaCloudDbContext>(options =>
-    options.UseSqlServer(connectionStringSecret));
+    options.UseSqlServer(connectionStringSecretValue));
 
 
 var app = builder.Build();
